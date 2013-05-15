04a75285421c88de2ecae19f1686bc6a36814fdd
Kamal <kamal.micheal@bibalex.org>
2013-05-15 13:42:51 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bhl::Application.config.secret_token = 'defb59b799df218b145241072cbd9acde66fc7b91553b3e8f7f318279730358fb444d48df55e556d5f75e4cf8830a42446502d9dc469667c0bd724b5c6295e16'
