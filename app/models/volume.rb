03a4c3683fec599178153e7c06c71fd61a28727d
Kamal <kamal.micheal@bibalex.org>
2013-05-27 13:53:18 +0200

    tunning scenario to include volumes

--- app/models/volume.rb
+++ app/models/volume.rb
@@ -1,4 +1,5 @@
 class Volume < ActiveRecord::Base
   belongs_to :book
-  attr_accessible :content_index_status, :copyright, :fill_content_fail, :generate_names_xml_fail, :get_pages_names_fail, :get_thumbnail_fail, :job_id, :name, :ubio_in_dar_fail, :volume_number
+  attr_accessible :content_index_status, :copyright, :fill_content_fail, :generate_names_xml_fail, :get_pages_names_fail 
+  attr_accessible :get_thumbnail_fail, :job_id, :name, :ubio_in_dar_fail, :volume_number, :book
 end
