149dc281be1b3ebe7e47ac028ab4f60e890f1c0a
Kamal <kamal.micheal@bibalex.org>
2013-06-03 10:03:23 +0200

    New constants to environment.rb

--- config/environment.sample.rb
+++ config/environment.sample.rb
@@ -10,6 +10,12 @@ DAR_API_METADATA = 'http://api.dar.bibalex.org/DarAccessLayer/[BIBID]/getStream/
 DAR_API_METADATA_BIBID_STRING = '[BIBID]'
 DOWNLOAD_QUERY_SIZE = 100
 
+DAR_API_THUMBNAIL = 'http://api.dar.bibalex.org/DarAccessLayer/DAF-Job:[JOBID]/Thumbnail'
+DAR_API_THUMBNAIL_JOBID_STRING = '[JOBID]'
+
+DAR_API_CONTENT = 'http://api.dar.bibalex.org/DarAccessLayer/DAF-Job:[JOBID]/Derivative/XML/Content';
+DAR_API_CONTENT_JOBID_STRING = '[JOBID]'
+
 LOCATION_API = "http://maps.googleapis.com/maps/api/geocode/xml?address=[LOC]&sensor=false"
 LOCATION_API_LOC_STRING = "[LOC]"
 
