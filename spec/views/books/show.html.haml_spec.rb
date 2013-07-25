ebc1134ba3814170273979066a50ac4667f46148
Kamal <kamal.micheal@bibalex.org>
2013-07-25 12:33:06 +0200

    Fixing specs + loading mods

---
+++ spec/views/books/show.html.haml_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe "books/show.html.haml" do
+  before (:all) do
+    truncate_all_tables
+    
+    doc = {:vol_jobid => "123", :bok_bibid => "456"}
+    doc[:bok_title] = "Test Book"
+    doc[:name] = "Test Name"
+    
+    solr = RSolr.connect :url => SOLR_BOOKS_METADATA
+    # remove this book if exists
+    solr.delete_by_query('vol_jobid:123')
+    solr.commit
+    solr.add doc
+    solr.commit
+    
+    book = Book.gen(:title => "Test Book", :bibid => "456", :mods => "<xml>xml content</xml>")
+    volume = Volume.gen(:book => book, :job_id => 123)
+  end
+  
+  it 'should contain tabs' do
+    visit "/books/123/brief"
+    page.should have_content(I18n.t(:brief))
+    page.should have_content(I18n.t(:mods))
+    page.should have_content(I18n.t(:bibtex))
+    page.should have_content(I18n.t(:endnote))
+  end
+  
+  it 'should contain book title' do
+    visit "/books/123/brief"
+    page.should have_content("Test Book")
+  end
+  
+  it 'should load mods' do
+    visit "/books/123/mods"
+    page.should have_content("xml content")
+  end
+end
