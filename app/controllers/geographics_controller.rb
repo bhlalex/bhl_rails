d41edd59921bf210e9c1254d583da07afea5b478
Kamal <kamal.micheal@bibalex.org>
2013-08-25 13:05:50 +0200

    WIP Maps

--- app/controllers/geographics_controller.rb
+++ app/controllers/geographics_controller.rb
@@ -1,9 +1,23 @@
 class GeographicsController < ApplicationController
   def index
     @page_title = 'Geographics'
+    
+    @map = Cartographer::Gmap.new( 'map' )
+    @map.zoom = 2
+    @map.icons << Cartographer::Gicon.new
+    
+    locations = Location.all
+    
+    locations.each do |location|
+      @map.markers << Cartographer::Gmarker.new(:name=> location.formatted_address.gsub(/\W/, "_"), :marker_type => "Building",
+                        :position => [location.latitude,location.longitude],
+                        :info_window_url => "#{location.id}")
+    end
   end
   
   def show
-    @page_title = 'Geographics'
+    location = Location.find_by_id(params[:id])
+    @location_name = location.formatted_address unless location.nil?
+    render :layout => 'main'
   end
 end
