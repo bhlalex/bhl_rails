a1d77c31ba584477504c1d68a6c3fea17600eae9
unknown <Kamal.Micheal@.local.bibalex.dom>
2013-09-05 08:13:38 +0200

    BHL-60 specs

--- spec/controllers/names_controller_spec.rb
+++ spec/controllers/names_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe NamesController do
+  render_views # we need it to be able to test response body content 
 
   describe "GET 'index'" do
     it "returns http success" do
@@ -10,10 +11,21 @@ describe NamesController do
   end
 
   describe "GET 'show'" do
-    it "returns http success" do
-      get 'show'
+    it "returns kingdoms" do
+      get "show"
       response.should be_success
+      response.body.include?('Animalia').should be_true
+      # I know this is a hard coded string, but this is a name of a kingdom, and I don't think it will ever change
     end
   end
-
+  
+  describe "GET 'get_content'" do
+    it "returns name of a taxon" do
+      he = HierarchyEntry.find_siblings(DEFAULT_HIERARCHY_ID, 0).first
+      get "get_content", :id => he.id
+      response.should be_success
+      response.body.include?(he.clean_taxon_concept).should be_true
+    end
+  end
+  
 end
