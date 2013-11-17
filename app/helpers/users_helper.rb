17fcd92fb7c9d863bbecb0c0f48d999cd5ad5af2
youstina <youstina@youstina.(none)>
2013-11-17 10:21:37 +0200

    Add user history

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,6 +4,8 @@ module UsersHelper
   end
   
   def set_view(params, view)
+    params[:controller] = nil
+    params[:action] = nil
     tmp_params = params.clone
     tmp_params[:view] = view
     tmp_params
