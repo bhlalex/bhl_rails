54db7da408e2ddd9c4442a48b2f18ce6889ab570
walaa <walaa@walaa-HP-Compaq-dc7700-Convertible-Minitower.(none)>
2013-11-18 10:04:20 +0200

    save query feature

---
+++ app/controllers/user_search_history_controller.rb
@@ -0,0 +1,19 @@
+class UserSearchHistoryController < ApplicationController
+  # walaa
+    def save_query
+      query = Query.new(:user_id => params[:user_id].to_i, :string => params[:query])
+      query.save
+      flash[:success] = "Query saved"
+      flash.keep
+      redirect_to :back
+    end
+  
+    def delete_query
+      query = Query.find(params[:id])
+      query.destroy
+      flash[:success]="Query destroyed"
+      flash.keep
+      redirect_to :back
+    end
+  # walaa
+end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
+
 require_relative '../../lib/bhl/login'
 
 include BHL::Login
 
 describe UsersController do
-  
+  render_views
   before(:each) do
     truncate_table(ActiveRecord::Base.connection, "users", {})
     User.gen() unless User.first
@@ -17,7 +18,7 @@ describe UsersController do
       get :new
       response.should be_success
     end
-    
+
     it 'should render new unless logged in' do
       get :new
       response.should render_template('users/new')
@@ -28,14 +29,76 @@ describe UsersController do
       expect(response).to redirect_to("/users/show/#{@user.id}")
     end
   end
-  
+
   describe 'GET show' do
+
     it 'should render show' do
       get :show, { :id => @user.id }
       response.should render_template('users/show')
     end
+
+    # saved queries test
+    describe "saved queries" do
+      before(:each) do
+        @query_first = Query.create(:user_id => @user.id, :string => "bok_title:(popular)", :created_at => Date.today)
+        @query_second = Query.create(:user_id => @user.id, :string => "content:(smith)", :created_at => Date.yesterday)
+      end
+
+      describe "saved queries header" do
+        it "should contains query content header" do
+          get :show, { :id => @user.id, :tab => "saved_queries" }
+          response.should have_selector('th', :content => "Query Content")
+        end
+
+        it "should contains query date header" do
+          get :show, { :id => @user.id, :tab => "saved_queries" }
+          response.should have_selector('th', :content => "Query Date")
+        end
+
+        it "should contains show result header" do
+          get :show, { :id => @user.id, :tab => "saved_queries" }
+          response.should have_selector('th', :content => "Show result")
+        end
+
+        it "should contains delete query header" do
+          get :show, { :id => @user.id, :tab => "saved_queries" }
+          response.should have_selector('th', :content => "Delete from history")
+        end
+
+        it "should contains query content body" do
+          get :show, { :id => @user.id, :tab => "saved_queries" }
+          response.should have_selector('td', :content => "bok_title:(popular)")
+          response.should have_selector('td', :content => "content:(smith)")
+        end
+      end
+
+        it "should contains query date body" do
+          get :show, { :id => @user.id, :tab => "saved_queries" }
+          response.should have_selector('td', :content => "bok_title:(popular)")
+        end
+
+        it "should contains show result link for query" do
+          get :show, { :id => @user.id, :tab => "saved_queries" }
+          response.should have_selector('a', :href => "/books?_bok_title=popular", :content => "show result")
+          response.should have_selector('a', :href => "/books?_content=smith", :content => "show result")
+        end
+
+        it "should contains delete link for each query" do
+          get :show, { :id => @user.id, :tab => "saved_queries" }
+          response.should have_selector('a', :href => "/user_search_history/delete_query?id=#{@query_first.id}", :content => "delete")
+          response.should have_selector('a', :href => "/user_search_history/delete_query?id=#{@query_second.id}", :content => "delete")
+        end
+
+        it "should have pagination bar" do
+          20.times {Query.create(:user_id => @user.id, :string => "search option:(search content)")}
+          get :show, { :id => @user.id, :tab => "saved_queries" }
+          response.should have_selector('ul', :id => "pagination")
+        end
+
+    end
+    # end
   end
-  
+
   describe 'GET forgot_password' do
     it 'should not render forget_password if logged in' do
       log_in(@user)
@@ -47,7 +110,7 @@ describe UsersController do
       response.should render_template('users/forgot_password')
     end
   end
-  
+
   describe 'POST recover_password' do
     it 'should not render forget_password if logged in' do
       log_in(@user)
@@ -55,24 +118,24 @@ describe UsersController do
       response.should_not render_template('users/forgot_password')
       expect(response).to redirect_to("/users/show/#{@user.id}")
     end
-    
+
     it "should find user by email or flash error if it can't find user by email" do
       post :recover_password, { :user => { :email => '' } }
       flash[:error].should_not be_blank
       expect(response).to redirect_to("/users/forgot_password")
       flash.clear
-      
+
       post :recover_password, { :user => { :email => 'blabla' } }
       expect(response).to redirect_to("/users/forgot_password")
       flash[:error].should_not be_blank
       flash.clear
-      
+
       post :recover_password, { :user => { :email => @user.email } }
       expect(response).to redirect_to("/users/login")
       flash[:error].should be_blank
       flash[:notice].should_not be_blank
     end
-    
+
     it 'should change verification activation code if valid email' do
       old_verification_code = @user.verification_code
       post :recover_password, { :user => { :email => @user.email } }
@@ -82,21 +145,21 @@ describe UsersController do
       @user.reload # I had to reload the object to reflect the verfication_code changes
       @user.verification_code.should_not == old_verification_code
     end
-    
+
   end
 
   describe 'POST create' do
     it 'should create a new user if registration is valid' do
       truncate_table(ActiveRecord::Base.connection, "users", {})
       # I need to make sure no users exist to make sure that the user is not duplicate
-      post :create, { :user => { 
-                                :username => 'test_user',
-                                :entered_password => 'test_password',
-                                :entered_password_confirmation => 'test_password',
-                                :email => 'test@email.com',
-                                :email_confirmation => 'test@email.com',
-                                :real_name => "Test User"
-                              } }
+      post :create, { :user => {
+        :username => 'test_user',
+        :entered_password => 'test_password',
+        :entered_password_confirmation => 'test_password',
+        :email => 'test@email.com',
+        :email_confirmation => 'test@email.com',
+        :real_name => "Test User"
+        } }
       response.should_not render_template('users/new') # request is invalid and redirected to form
       @user = User.first
       @user.should_not be_nil
@@ -113,13 +176,13 @@ describe UsersController do
       truncate_table(ActiveRecord::Base.connection, "users", {})
       User.gen(:username => 'test_user')
       post :create, { :user => {
-                                :username => 'test_user',
-                                :entered_password => 'test_password',
-                                :entered_password_confirmation => 'test_password',
-                                :email => 'test@email.com',
-                                :email_confirmation => 'test@email.com',
-                                :real_name => "Test User"
-                              } }
+        :username => 'test_user',
+        :entered_password => 'test_password',
+        :entered_password_confirmation => 'test_password',
+        :email => 'test@email.com',
+        :email_confirmation => 'test@email.com',
+        :real_name => "Test User"
+        } }
       response.should render_template('users/new') # request is invalid and redirected to form
     end
   end
@@ -129,28 +192,28 @@ describe UsersController do
       user = User.gen
       user.active.should be_false
     end
-    
+
     it 'should raise flash error and redirect to home page if user is already activated or invalid parameters' do
       user = User.gen
-      
+
       get :activate, { :guid => "empty", :activation_code => "empty" }
       response.should redirect_to(root_path)
       flash[:error].should_not be_blank
-      
+
       get :activate, { :guid => "empty", :activation_code => user.verification_code }
       response.should redirect_to("/")
       flash[:error].should_not be_blank
-      
+
       get :activate, { :guid => user.guid, :activation_code => "empty"}
       response.should redirect_to("/")
       flash[:error].should_not be_blank
-      
+
       user.activate
       get :activate, { :guid => user.guid, :activation_code => user.verification_code }
       response.should redirect_to("/")
       flash[:error].should_not be_blank
     end
-    
+
     it 'should activate user and redirect to home page' do
       user = User.gen
       get :activate, { :guid => user.guid, :activation_code => user.verification_code }
@@ -160,7 +223,7 @@ describe UsersController do
       user.reload
       user.active.should be_true
     end
-    
+
     it 'should update active in session variables after activation' do
       user = User.gen
       log_in(user)
@@ -181,7 +244,7 @@ describe UsersController do
       is_loggged_in?.should be_false
     end
   end
-  
+
   describe 'GET login' do
     it 'should redirect to profile page if user is logged in' do
       user = User.gen
@@ -189,13 +252,13 @@ describe UsersController do
       get :login
       response.should redirect_to "/users/show/#{user.id}"
     end
-    
+
     it 'should render login form' do
       get :login
       response.should render_template("users/login")
     end
   end
-  
+
   describe 'POST validate' do
     it 'should redirect to profile page if user is logged in' do
       log_in(@user)
@@ -203,7 +266,7 @@ describe UsersController do
       post :validate, { :user => { :username => user.username, :password => "1234" } }
       response.should redirect_to "/users/show/#{@user.id}"
     end
-    
+
     it 'should validate user, set session, and redirect to profile page' do
       user = User.gen(:entered_password => "1234")
       post :validate, { :user => { :username => user.username, :password => "1234" } }
@@ -217,7 +280,7 @@ describe UsersController do
       flash[:notice].should_not be_blank
       is_loggged_in?.should be_true
     end
-    
+
     it 'should not validate user, and redirect to login page' do
       user = User.gen(:entered_password => "1234")
       post :validate, { :user => { :username => user.username, :password => "4567" } }
@@ -227,7 +290,7 @@ describe UsersController do
       is_loggged_in?.should be_false
     end
   end
-  
+
   describe 'GET reset_password' do
     it 'should redirect to profile page if user is logged in' do
       log_in(@user)
@@ -235,14 +298,14 @@ describe UsersController do
       get :reset_password, :guid => @user.guid, :activation_code => @user.verification_code
       response.should redirect_to "/users/show/#{@user.id}"
     end
-    
+
     it 'should raise error and redirect to root if invalid parameters' do
       user = User.gen(:entered_password => "1234")
       get :reset_password, :guid => @user.guid, :activation_code => "1234"
       response.should redirect_to "/"
       flash[:error].should_not be_blank
     end
-    
+
     it 'should render form if parameters are valid' do
       user = User.gen(:entered_password => "1234")
       get :reset_password, :guid => @user.guid, :activation_code => @user.verification_code
@@ -250,43 +313,43 @@ describe UsersController do
       flash[:error].should be_blank
     end
   end
-  
+
   describe 'POST reset_password_action' do
     it 'should redirect to profile page if user is logged in' do
       log_in(@user)
       post :reset_password_action
       response.should redirect_to "/users/show/#{@user.id}"
     end
-    
+
     it 'should redirect to home page if invalid parameters' do
       post :reset_password_action, { :user => { } }
       response.should redirect_to "/"
       flash[:error].should_not be_blank
-      
+
       flash.clear
-      
+
       post :reset_password_action, { :user => { :guid => @user.guid, :activation_code => "1234" } }
       response.should redirect_to "/"
       flash[:error].should_not be_blank
     end
-    
+
     it 'should apply password validators before update' do
-      post :reset_password_action, { :user => { 
-                                                :guid => @user.guid, 
-                                                :activation_code => @user.verification_code,
-                                                :entered_password => "123",
-                                                :entered_password_confirmation => "123"} }
+      post :reset_password_action, { :user => {
+        :guid => @user.guid,
+        :activation_code => @user.verification_code,
+        :entered_password => "123",
+        :entered_password_confirmation => "123"} }
       response.should redirect_to "/users/reset_password/#{@user.guid}/#{@user.verification_code}"
       flash[:error].should_not be_blank
     end
-    
+
     it 'should should change password and redirect to login' do
       old_password = @user.password
-      post :reset_password_action, { :user => { 
-                                                :guid => @user.guid, 
-                                                :activation_code => @user.verification_code,
-                                                :entered_password => "1234",
-                                                :entered_password_confirmation => "1234"} }
+      post :reset_password_action, { :user => {
+        :guid => @user.guid,
+        :activation_code => @user.verification_code,
+        :entered_password => "1234",
+        :entered_password_confirmation => "1234"} }
       @user.reload
       @user.password.should_not == old_password
       @user.password.should == Digest::MD5.hexdigest("1234")
@@ -295,13 +358,13 @@ describe UsersController do
       flash[:notice].should_not be_blank
     end
   end
-  
+
   describe 'GET edit' do
     it 'should redirect to login page if user is not logged_in' do
       get :edit, :id => @user.id
       response.should redirect_to "/users/login"
     end
-    
+
     it 'should redirect to profile page if editing different user' do
       user = User.gen
       log_in(@user)
@@ -309,7 +372,7 @@ describe UsersController do
       response.should redirect_to "/users/show/#{user.id}"
       flash[:error].should_not be_blank
     end
-    
+
     it 'should render edit form' do
       log_in(@user)
       get :edit, :id => @user.id
@@ -317,13 +380,13 @@ describe UsersController do
       flash[:error].should be_blank
     end
   end
-  
+
   describe 'PUT update' do
     it 'should redirect to login page if user is not logged_in' do
       put :update, { :id => @user.id }
       response.should redirect_to "/users/login"
     end
-    
+
     it 'should redirect to profile page if editing different user' do
       user = User.gen
       log_in(@user)
@@ -331,37 +394,37 @@ describe UsersController do
       response.should redirect_to "/users/show/#{user.id}"
       flash[:error].should_not be_blank
     end
-    
+
     it 'should accept modifications even if the password is empty' do
       user_before_update = @user
       log_in(@user)
       put :update, { :id => @user.id ,:user => { :id => @user.id, :username => user_before_update.username,
-                                                                  :email => user_before_update.email,
-                                                                  :email_confirmation => user_before_update.email,
-                                                                  :entered_password => nil,
-                                                                  :entered_password_confirmation => nil,
-                                                                  :real_name => user_before_update.real_name + "updated"}}
+        :email => user_before_update.email,
+        :email_confirmation => user_before_update.email,
+        :entered_password => nil,
+        :entered_password_confirmation => nil,
+        :real_name => user_before_update.real_name + "updated"}}
       expect(response).to redirect_to "/users/show/#{@user.id}"
       flash[:error].should be_blank
       flash[:notice].should_not be_blank
-      
+
       @user.reload
-      
+
       @user.username.should == user_before_update.username
       @user.password.should == user_before_update.password
       @user.email.should == user_before_update.email
       @user.real_name == user_before_update.real_name + "updated"
       session[:real_name].should == @user.real_name
     end
-    
+
     it 'should reject modifications and redner edit for invalid password' do
       log_in(@user)
       put :update, { :id => @user.id ,:user => { :id => @user.id, :username => @user.username,
-                                                                  :email => @user.email,
-                                                                  :email_confirmation => @user.email,
-                                                                  :entered_password => "123",
-                                                                  :entered_password_confirmation => "123",
-                                                                  :real_name => @user.real_name}}
+        :email => @user.email,
+        :email_confirmation => @user.email,
+        :entered_password => "123",
+        :entered_password_confirmation => "123",
+        :real_name => @user.real_name}}
       expect(response).to render_template "users/edit"
     end
   end
