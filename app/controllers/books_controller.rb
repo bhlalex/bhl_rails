303d5166bb0c27931b9aeefa4dd9aa5fa1c099e4
walaa <walaa@walaa-HP-Compaq-dc7700-Convertible-Minitower.(none)>
2014-01-12 08:46:30 +0200

    test

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,6 @@ class BooksController < ApplicationController
   include BHL::Login
   
   def index
-    debugger
     @user_history = UserBookHistory.where(:user_id => session[:user_id])
     @url_params = fix_dar_url(params)
     @page_title = I18n.t(:search_results_colon)
