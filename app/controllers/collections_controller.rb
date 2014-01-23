f8326021f6ed71ed4ea25be56591ba695aac0c08
youstina <youstina@youstina.(none)>
2014-01-23 16:26:53 +0200

    remove hard coded strings

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -223,7 +223,7 @@ class CollectionsController < ApplicationController
       @results << item.title
     end 
     if (@results.length == 0)
-      @results << "No Suggestion"
+      @results << "#{I18n.t(:no_suggestion)}"
     end
     render json: @results
   end
