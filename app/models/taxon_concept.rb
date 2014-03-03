d66d542253bb7b0657b80400c22c4a5582d86486
Kamal Micheal <kamal@kamal-VirtualBox.(none)>
2014-03-03 15:45:18 +0200

    imroving eol harvesting

--- app/models/taxon_concept.rb
+++ app/models/taxon_concept.rb
@@ -3,6 +3,31 @@ class TaxonConcept < EOLBase
   
   attr_accessible :id, :name, :image
   
+  def self.find_taxon_for_name(clean_name)    
+    # first we need to get name_id
+    name_id = self.get_name_id_from_clean_name(clean_name)
+    
+    return nil if name_id.nil?
+
+    # next step is to get taxon_concept_id associated with this name
+    tc = self.find_by_sql("select taxon_concept_id from taxon_concept_names where name_id=#{name_id} order by preferred desc limit 1;")
+
+    return nil if tc.count <= 0
+
+    taxon_concept_id = tc.first.taxon_concept_id
+
+    return taxon_concept_id
+  end
+
+  def self.get_name_id_from_clean_name(clean_name)
+    names = self.find_by_sql("select id from names where string='#{clean_name}' or clean_name='#{clean_name}' limit 1;")
+    return nil if names.count <= 0
+
+    # now we found a name
+    name_id = names.first.id
+    return name_id
+  end
+
   def self.get_name(id)
     tc = self.find_by_sql("select names.string as name from names 
                             inner join taxon_concept_names tcn 
@@ -21,6 +46,7 @@ class TaxonConcept < EOLBase
     else
       tc = self.find_by_sql("select object_cache_url as image from top_concept_images tci 
                               inner join data_objects do on do.id=tci.data_object_id and tci.taxon_concept_id=#{id} 
+                              where (data_subtype_id is null or data_subtype_id<>10)
                              order by view_order limit 1")
       if tc.count >= 1
         return tc.first.image
