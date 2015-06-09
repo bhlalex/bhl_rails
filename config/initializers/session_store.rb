d9267ebd702fa237fc4adee7ddaa0913fa343b6d
wala2.elsayed <walaa.elsayed@bibalex.org>
2015-06-09 16:59:37 +0300

    update session timeout

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Bhl::Application.config.session_store :cookie_store, key: '_bhl_session'
+Bhl::Application.config.session_store :cookie_store, key: '_bhl_session', expire_after: 10.minute
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
